4a01d8c03a606acea71cf8b52b7d3c3c1c5d9087
jjhu94 <709259187@qq.com>
2019-12-05 17:02:57 +0800

    test

---
+++ neural_networks.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+###NEURAL NETWORKS####
+# STEP1: Define the network
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()  # inherit all the variables defined in parent class
+        self.conv1 = nn.Conv2d(1, 6, 3)  # 1 sample, set to 6 3*3 filters output
+        self.conv2 = nn.Conv2d(6, 16, 3)  # 1 input filters from last layer, set to 16 3*3 filters output
+        self.fc1 = nn.Linear(16 * 6 * 6,
+                             120)  # 6*6 is the H and W of the result picture after 2 conv, set to 120 length
+        self.fc2 = nn.Linear(120, 84)  # 120 input length of linear, 84 output
+        self.fc3 = nn.Linear(84, 10)  # 10 output
+
+    def forward(self, values):  # just have to define the forward , backward one is automatically defined by autograd.
+        values = F.max_pool2d(F.relu(self.conv1(values)), 2)  # layer1: convolution, relu, pool
+        values = F.max_pool2d(F.relu(self.conv2(values)), 2)  # layer2: convolution, relu, pool
+        values = values.view(-1, self.num_flat_features(values))  # flatten: defined below;
+        # the size -1 means it is inferred from other dimensions of the Tensor
+        values = F.relu(self.fc1(values))  # layer3: full connection, linear -> relu
+        values = F.relu(self.fc2(values))  # layer4: full connection, linear -> relu
+        values = self.fc3(values)  # layer5: have activation function such as softmax or sigmoid to classify the outputs
+        return values
+
+    def num_flat_features(self, values):  # to get the total features
+        size = values.size()[1:]  # get all dimensions except the batch one ([0])
+        num_features = 1
+        for s in size:
+            num_features *= s  # to multiple all features and get the result
+        return num_features
+
+
+net = Net()
+print(net)  # get the structure of the NN
+
+# get learnable parameters: net.parameters()
+params = list(net.parameters())  # The learnable parameters of a model are returned by net.parameters()
+print(len(params))
+for i in range(0, 9):
+    print(params[i].size())  # for example:  conv1's .weight:torch.Size([6, 1, 3, 3])
+    # 6 filters, 1 input, 3*3 for H and W
+
+# Example: Processing inputs: try a random 32x32 input.
+input = torch.randn(1, 1, 32, 32)
+out = net(input)
+print(out)
+
+# Example: Zero the gradient buffers of all parameters and backprops with random gradients
+net.zero_grad()  # set the gradient to 0
+out.backward(torch.randn(1, 10))  # backprops with random gradients
+
+# Example: for Loss Function and backward
+# Loss Function
+output = net(input)
+target = torch.randn(1, 10)
+criterion = nn.MSELoss()
+loss = criterion(output, target)
+print(loss)
+# backward
+"""
+Now, if you follow loss in the backward direction, using its .grad_fn attribute, 
+you will see a graph of computations that looks like this
+input -> conv2d -> relu -> maxpool2d -> conv2d -> relu -> maxpool2d
+      -> view -> linear -> relu -> linear -> relu -> linear
+      -> MSELoss
+      -> loss
+So, when we call loss.backward(), the whole graph is differentiated w.r.t. the loss, and all Tensors in the graph 
+that has requires_grad=True will have their .grad Tensor accumulated with the gradient.
+For illustration, let us follow a few steps backward:
+"""
+print(loss.grad_fn)  # MSELoss
+print(loss.grad_fn.next_functions[0][0])  # Linear
+print(loss.grad_fn.next_functions[0][0].next_functions[0][0])  # ReLU
+
+# Backprop
+net.zero_grad()  # zeroes gradient buffers of all parameters: else gradients will be accumulated to existing gradients.
+loss.backward()
+
+# Update the weights: use torch.optim
+optimizer = optim.SGD(net.parameters(), lr=0.01)  # lr: learning rate
+# in your training loop:
+optimizer.zero_grad()   # zero the gradient buffers
+output = net(input)  # see STEP1: define a neural network
+loss = criterion(output, target)  # STEP2: compute loss
+loss.backward()
+optimizer.step()    # STEP3: Does the update
---
+++ training_a_classifier_on_GPU.py
@@ -0,0 +1,93 @@
+import torch
+import torchvision  # the package has data loaders for common datasets
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+transform = transforms.Compose([
+    # transforms.Resize((32, 32)),  # used when the image is not 32*32
+    transforms.ToTensor(),  # turn data to [0,1] and to Tensor
+    transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))])  # normalize: to [-1.0, 1.0] (by (x-0.5)/0.5)
+trainset = torchvision.datasets.CIFAR10(root="./data", train=True,
+                                        download=True, transform=transform)
+# download train and transform it as set, root means the place to save data
+trainloader = torch.utils.data.DataLoader(dataset=trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+# shuffle set to True for trainset and set to False for testset; reshuffle it in each epoch
+# num_workers: use subprocesses to read data; batch_size: 4 in 1 batch
+testset = torchvision.datasets.CIFAR10(root="./data", train=False,
+                                       download=False, transform=transform)  # only need to download once
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')  # name classes
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)  # 3-channel images, 6 filters with 5*5 H & W
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # the result image with H and W at 5*5
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+net.to(device)  # use net on GPU
+
+### Define a Loss function and optimizer ####
+criterion = nn.CrossEntropyLoss()  # define loss function: classification cross-entropy loss
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)  # optimizer: use SGD with momentum update the weights
+# create an optimizer object, maintain current parameter status and update them based on calculated gradients
+
+### Train the network ###
+for epoch in range(2):  # loop times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader):
+        inputs, labels = data[0].to(device), data[1].to(device)  # send the inputs and targets to the GPU AT EVERY STEP
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)  # forward
+        loss.backward()  # backward: calculate gradients
+        optimizer.step()  # optimize: update all parameters based on calculated gradients
+        # simplied version, notice that it is only used once in one mini-batch
+
+        running_loss += loss.item()  # accumulate the loss; .item() is used to get the values of the object
+        if i % 2000 == 1999:
+            print("[%d, %5d] loss: %.3f" % (epoch + 1, i + 1, running_loss / 2000))  # print the mean loss
+            running_loss = 0  # set to 0 for another loop
+print("finished")
+
+# save trained model
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+class_correct = list(0. for i in range(10))  # what the . mean?
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data[0].to(device), data[1].to(device)
+        outputs = net(images)
+        _, predicted_label = torch.max(outputs, 1)
+        a = (predicted_label == labels)
+        c = (predicted_label == labels).squeeze()  # delect all dimensions with only 1 object
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()  # plus 0 or 1 for the specific class
+            class_total[label] += 1
+for i in range(10):
+    print('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
+
